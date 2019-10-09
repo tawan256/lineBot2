a527a579b98ef78418803361048ea358e9ff408e
tawan256 <56185065+tawan256@users.noreply.github.com>
2019-10-09 09:28:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,7 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
-    userText = decoded["events"][0]['message']['text']
-    #userText = decoded["events"][0]['message']['text']
+    user = decoded['originalDetectIntentRequest'][payload]['data']['replyToken']
     userText = decoded['queryResult']['intent]['displayName']
     #sentText(user,userText)
    if (userText == 'อยู่ป่าว') ;
