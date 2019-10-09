fba81548caca6ff443e9b7a44be02aa2768792d7
tawan256 <56185065+tawan256@users.noreply.github.com>
2019-10-09 10:51:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ def callback():
     userText = decoded['queryResult']['intent']['displayName']
     #sentText(user,userText)
    if (userText == 'อยู่ป่าว') :
-      sendText(user, 'อยู่สิ')
+      (sendText(user, 'อยู่สิ')
    elif (userText == 'สบายดีมั้ย') :
-      sendText(user, 'ยังสบายดีอยู๋')
+      (sendText(user, 'ยังสบายดีอยู๋')
    else :
        sendText(user, 'พิมพ์อะไรมาเนี่ย ไม่เข้าใจ')                                
 
